4d6fcf1ee4931b373ab35d26945f8d041a983099
liamn2011 <liamn2011@gmail.com>
2023-11-29 18:11:40 +0000

    emailForm

--- src/components/emailForm.js
+++ src/components/emailForm.js
@@ -17,19 +17,13 @@ const EmailForm = () => {
 	const [canSubmit, setCanSubmit] = useState(false);
 
 	const validateField = (e) => {
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 		const enteredValue = e.target.value;
 		const fieldName = e.target.name;
 
 		setSubmitError(false);
 
-		if (fieldName === "email") {
-			const isValidEmail = enteredValue.trim() !== "" && emailRegex.test(enteredValue);
-			setErrors({ ...errors, [fieldName]: !isValidEmail });
-		} else {
-			const isValidField = enteredValue.trim() !== "";
-			setErrors({ ...errors, [fieldName]: !isValidField });
-		}
+		const isValidField = enteredValue.trim() !== "";
+		setErrors({ ...errors, [fieldName]: !isValidField });
 
 		if (!errors.name && !errors.email && !errors.message) {
 			setCanSubmit(true);
@@ -38,7 +32,7 @@ const EmailForm = () => {
 
 	const sendEmail = async (name, email, message) => {
 		try {
-			const response = await axios.post("https://formsubmit.co/ajax/3604cbecb7c1e2851ef1ee073691361e", {
+			const response = await axios.post("https://formsubmit.co/ajax/lwnorton@mail.com", {
 				name: name,
 				email: email,
 				message: message,
@@ -50,11 +44,14 @@ const EmailForm = () => {
 	};
 
 	const submit = async () => {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const name = document.getElementById("name").value;
+		const email = document.getElementById("email").value;
+		const message = document.getElementById("message").value;
 		setErrors({ ...errors, submit: false });
-		if (!errors.email === true && !errors.name === true && !errors.message === true) {
-			const name = document.getElementById("name").value;
-			const email = document.getElementById("email").value;
-			const message = document.getElementById("message").value;
+		if (!emailRegex.test(email) && email !== "") {
+			setErrors({ ...errors, email: true });
+		} else if (!errors.email === true && !errors.name === true && !errors.message === true && emailRegex.test(email)) {
 			if (canSubmit && name !== "" && email !== "" && message !== "") {
 				setFormDisplay(false);
 				setLoading(true);
@@ -68,26 +65,26 @@ const EmailForm = () => {
 					setFormDisplay(true);
 					setSubmitError(true);
 				}
-			} else {
-				setSubmitError(true);
 			}
+		} else {
+			setSubmitError(true);
 		}
 	};
 
 	return (
 		<>
 			{formDisplay && (
-				<div className='flex-1 flex items-center justify-center flex-col'>
-					<div className='md:w-2/5'>
+				<div className='flex-1 flex items-center justify-center flex-col '>
+					<div className='md:w-2/5 w-11/12'>
 						<h1 className='text-white font-bold text-2xl mb-1'>CONTACT ME</h1>
-						<div className=' h-4 w-20 bg-gray-800 mb-1'></div>
+						<div className=' h-3 w-44 bg-gray-800 mb-1'></div>
 						<p className='text-white mb-4'>
 							If you wish to reach out to inquire more about me, share an exciting opportunity, or explore potential collaboration, kindly use the
 							contact form below. I look forward to hearing from you.
 						</p>
 					</div>
 
-					<div className='bg-gray-800 p-4 rounded-xl md:w-2/5 w-4/5'>
+					<div className='bg-gray-800 p-4 rounded-xl md:w-2/5  w-11/12'>
 						<label htmlFor='name' className='text-white'>
 							Name*
 						</label>
@@ -98,6 +95,7 @@ const EmailForm = () => {
 							className='block w-full my-2 rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 							placeholder='Name'
 							onBlur={validateField}
+							onChange={validateField}
 							required
 						/>
 						{errors.name && <p className='text-center mt-2 text-red-600 text-base font-bold'>Please enter your name</p>}
@@ -111,9 +109,10 @@ const EmailForm = () => {
 							className='block w-full my-2 rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 							placeholder='Email'
 							onBlur={validateField}
+							onChange={validateField}
 							required
 						/>
-						{errors.email && <p className='text-center mt-2 text-red-600 text-base font-bold'>Email is invalid, please enter a valid email</p>}
+						{errors.email && <p className='text-center mt-2 text-red-600 text-base font-bold'>Please enter a valid email</p>}
 						<label htmlFor='message' className='text-white'>
 							Message*
 						</label>
@@ -121,6 +120,7 @@ const EmailForm = () => {
 							id='message'
 							name='message'
 							rows={3}
+							maxlength='500'
 							className='block w-full my-2 rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 							defaultValue={""}
 							placeholder='Message'
@@ -130,7 +130,7 @@ const EmailForm = () => {
 						/>
 						{errors.message && <p className='text-center mt-2 text-red-600 text-base font-bold'>Please enter a message</p>}
 						{submitError && !errors.name && !errors.email && !errors.message && (
-							<p className='text-center mt-2 text-red-600 text-base font-bold'>Please complete the form</p>
+							<p className='text-center mt-2 text-red-600 text-base font-bold'>Please check that all fields a completed correctly</p>
 						)}
 						<button
 							onClick={submit}
