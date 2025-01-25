34562056f9f1457b39fac992a84bc814c1c9ac1b
liamn2011 <liamn2011@gmail.com>
2025-01-25 18:30:07 +0000

    Phase 2: Update job description, disable cv button and contact me button when form is not fully populated

--- src/components/emailForm/sumbitButton/submitButton.js
+++ src/components/emailForm/sumbitButton/submitButton.js
@@ -53,7 +53,8 @@ const SubmitButton = () => {
 				className={clsx(
 					"rounded-md bg-gray-800 border-white border-2 px-3 py-2 mt-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white",
 					{
-						" opacity-20": isDisabled,
+						"opacity-20": isDisabled,
+						"cursor-not-allowed": isDisabled,
 					}
 				)}>
 				Submit
