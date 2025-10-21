593d48decc7daf103f4dd9f813387bf672984d4b
liamn2011 <liamn2011@gmail.com>
2025-10-21 06:57:17 +0100

    make copyright date dynamic

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -4,25 +4,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-	return (
-		<footer className=' px-4 py-2 w-full bg-gray-800 mt-3'>
-			<div className='container mx-auto  flex justify-between md:justify-evenly items-center '>
-				<p className='text-white'>
-					<span>&copy;</span> Copyright 2024 Liam Norton.
-				</p>
-				<div>
-					<a href='https://github.com/liamn2011' aria-label='Github' target='_blank' rel='noreferrer' className=''>
-						<FontAwesomeIcon icon={faGithub} size='2xl' style={{ color: "#ffffff" }} />
-					</a>
-					<a href='https://www.linkedin.com/in/liam-norton-335139b3/' aria-label='LinkedIn' target='_blank' rel='noreferrer' className='ml-2'>
-						<FontAwesomeIcon icon={faLinkedin} size='2xl' style={{ color: "#ffffff" }} />
-					</a>
-				</div>
+  const d = new Date();
+  return (
+    <footer className=" px-4 py-2 w-full bg-gray-800 mt-3">
+      <div className="container mx-auto  flex justify-between md:justify-evenly items-center ">
+        <p className="text-white">
+          <span>&copy;</span> Copyright {d.getFullYear()} Liam Norton.
+        </p>
+        <div>
+          <a
+            href="https://github.com/liamn2011"
+            aria-label="Github"
+            target="_blank"
+            rel="noreferrer"
+            className=""
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="2xl"
+              style={{ color: "#ffffff" }}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/liam-norton-335139b3/"
+            aria-label="LinkedIn"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-2"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2xl"
+              style={{ color: "#ffffff" }}
+            />
+          </a>
+        </div>
 
-				{/* Mobile Menu */}
-			</div>
-		</footer>
-	);
+        {/* Mobile Menu */}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
